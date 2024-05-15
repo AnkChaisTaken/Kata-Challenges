12fc3f599a3ef06def389059be32078cba91a721
AnkChaisTaken <siyasetcienes@gmail.com>
2024-05-15 21:02:20 +0300

    ---

---
+++ KataChallenges/ListRotator.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;   
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataChallenges
+{
+    public static class ListRotator
+    {
+        public static void RotateListBy<T>(List<T> list, int k)
+        {
+
+        }
+    }
+}
--- KataChallenges/Program.cs
+++ KataChallenges/Program.cs
@@ -9,6 +9,11 @@ namespace KataChallenges
         {
             Console.WriteLine(RunLength.Encode("AAAABBBCCDAA"));
             Console.WriteLine(string.Join(',', SubarraySum.FindCoolContiguousElementsSumTo(new[] { 1, 2, 3, 4, 5 }, 9)));
+
+            List<int> sample = Enumerable.Range(1, 7).ToList();
+            ListRotator.RotateListBy<int>(sample, 4);
+
+            Console.WriteLine(string.Join(',', sample));
         }
     }
 }
