3df28bdac712cbad99c271d9bf97813a1a07e619
AnkChaisTaken <siyasetcienes@gmail.com>
2024-05-21 20:49:03 +0300

    4

--- KataChallenges/Program.cs
+++ KataChallenges/Program.cs
@@ -7,13 +7,23 @@ namespace KataChallenges
     {
         static void Main(string[] args)
         {
+            // Encoding/Decoding Kata
             Console.WriteLine(RunLength.Encode("AAAABBBCCDAA"));
+
+            // Subarray Sum
             Console.WriteLine(string.Join(',', SubarraySum.FindCoolContiguousElementsSumTo(new[] { 1, 2, 3, 4, 5 }, 9)));
 
+            // Rotate a List by K Elements
             int[] sample = Enumerable.Range(1, 6).ToArray();
             ListRotator.RotateListByLeft(sample, 2);
 
             Console.WriteLine(string.Join(',', sample));
+
+            // Reverse Integer
+            Console.WriteLine(321.Reverse());
+            Console.WriteLine(-120.Reverse());
+            Console.WriteLine(2147483647.Reverse());
+            Console.WriteLine(-2147483647.Reverse());
         }
     }
 }
---
+++ KataChallenges/ReverseInteger.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KataChallenges
+{
+    public static class ReverseInteger
+    {
+        public static int Reverse(this int x)
+        {
+            int sign = Math.Sign(x);
+            x = Math.Abs(x);
+
+            int number = 0;
+
+            while (x > 0)
+            {
+                var digit = x % 10;
+                x /= 10;
+
+                try
+                {
+                    number = checked((number * 10) + digit);
+                } catch (OverflowException e)
+                {
+                    return 0;
+                }
+            }
+
+            return number * sign;
+        }
+    }
+}
